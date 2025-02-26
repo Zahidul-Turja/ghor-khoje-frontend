23482b0b0fb0a13af036d63824a4f9f4bc400460
Zahidul-Turja <zahidulturja@gmail.com>
2025-02-26 18:48:54 +0600

    Add Footer component and adjust Home layout with padding

---
+++ app/_components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full bg-gray-800 py-4 text-center text-gray-100">
+      Footer
+    </footer>
+  );
+}
+
+export default Footer;
--- app/_components/pages/Home.jsx
+++ app/_components/pages/Home.jsx
@@ -1,11 +1,15 @@
 import SectionHeader from "@/app/_components/home/SectionHeader";
 import SectionProperties from "../home/SectionProperties";
+import Footer from "@/app/_components/Footer";
 
 export default function Home() {
   return (
     <>
-      <SectionHeader />
-      <SectionProperties />
+      <div className="px-16">
+        <SectionHeader />
+        <SectionProperties />
+      </div>
+      <Footer />
     </>
   );
 }
