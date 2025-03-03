cb918e546d8ef6d6b1bffccab117cad89fe76772
Zahidul-Turja <zahidulturja@gmail.com>
2025-03-03 21:33:27 +0600

    Add Page and BoundingBox components for layout structure

---
+++ app/_components/BoundingBox.jsx
@@ -0,0 +1,5 @@
+function BoundingBox({ children }) {
+  return <div>{children}</div>;
+}
+
+export default BoundingBox;
--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,7 +1,32 @@
 function Footer() {
   return (
     <footer className="w-full bg-gray-800 py-4 text-center text-gray-100">
-      Footer
+      <div className="flex items-center justify-between gap-4 px-64 py-12 text-left">
+        <h1 className="text-2xl font-extrabold uppercase text-primary">
+          Ghor Khoje
+        </h1>
+
+        <div>
+          <h2 className="mb-2 text-lg font-bold">Services</h2>
+          <ul>
+            <li>Buy</li>
+            <li>Rent</li>
+            <li>Sell</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="mb-2 text-lg font-bold">Navigation</h2>
+          <ul>
+            <li>Places</li>
+            <li>About us</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+
+      <p className="text-center text-sm font-light">
+        Copyright &copy; 2023 <span className="font-bold">Ghor Khoje</span>
+      </p>
     </footer>
   );
 }
