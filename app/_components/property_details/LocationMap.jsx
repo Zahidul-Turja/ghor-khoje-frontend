da288b3d716ecd32c8f697fbb078e2f0413e5a63
Zahidul-Turja <zahidulturja@gmail.com>
2025-03-17 12:01:51 +0600

    Add Google Maps button to LocationMap component and improve layout

--- app/_components/property_details/LocationMap.jsx
+++ app/_components/property_details/LocationMap.jsx
@@ -5,10 +5,9 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useEffect } from "react";
 
-// Fix for marker icons
 function LocationMap({ lat, lng }) {
   useEffect(() => {
-    // This code needs to run only on the client side
+    // Fix for marker icons
     delete L.Icon.Default.prototype._getIconUrl;
 
     L.Icon.Default.mergeOptions({
@@ -21,20 +20,35 @@ function LocationMap({ lat, lng }) {
     });
   }, []);
 
+  // Function to open location in Google Maps
+  const openInGoogleMaps = () => {
+    const googleMapsUrl = `https://www.google.com/maps?q=${lat},${lng}`;
+    window.open(googleMapsUrl, "_blank");
+  };
+
   return (
-    <MapContainer
-      center={[lat, lng]}
-      zoom={13}
-      style={{ height: "400px", width: "100%" }}
-    >
-      {/* OpenStreetMap Tile Layer (Free) */}
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[lat, lng]}>
-        <Popup>
-          Location: {lat}, {lng}
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="space-y-4">
+      <MapContainer
+        center={[lat, lng]}
+        zoom={13}
+        style={{ height: "400px", width: "100%" }}
+      >
+        {/* OpenStreetMap Tile Layer (Free) */}
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={[lat, lng]}>
+          <Popup>
+            Location: {lat}, {lng}
+          </Popup>
+        </Marker>
+      </MapContainer>
+
+      <button
+        onClick={openInGoogleMaps}
+        className="mt-4 cursor-pointer rounded-lg border border-gray-600 px-4 py-2 text-sm font-semibold"
+      >
+        <span>Open in Google Maps</span>
+      </button>
+    </div>
   );
 }
 
