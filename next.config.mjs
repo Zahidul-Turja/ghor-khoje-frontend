a317b58d54b1455c8b9b2ec39ea22036b9c9c95f
Zahidul-Turja <zahidulturja@gmail.com>
2025-05-25 15:56:14 +0600

    feat: Update image domains in next.config.mjs to include 'ghor-khoje-backend.onrender.com'

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-<<<<<<< HEAD
-    domains: ["127.0.0.1", "res.cloudinary.com"],
-=======
-    domains: ["127.0.0.1", "ghor-khoje-backend.onrender.com"],
->>>>>>> bf6cedaf3dbd2ca3a79fecfcf48cf411ed773f21
+    domains: [
+      "127.0.0.1",
+      "res.cloudinary.com",
+      "ghor-khoje-backend.onrender.com",
+    ],
   },
 };
 
