642fcd21c2dd5a59e712d9666ddf795af0101aa8
turja <zahidulturja@gmail.com>
2025-06-25 15:33:39 +0600

    feat: Add ProfileInfoCard and ProfileReviewsCard components for host information and performance metrics display

---
+++ app/_components/host_page/ProfileInfoCard.jsx
@@ -0,0 +1,56 @@
+import { SiVerizon } from "react-icons/si";
+import { RxCross2 } from "react-icons/rx";
+import Link from "next/link";
+import { HiMiniFlag } from "react-icons/hi2";
+
+function ProfileInfoCard({ host }) {
+  return (
+    <div>
+      <div className="w-full rounded-2xl border border-gray-300 p-4">
+        <h3 className="text-lg font-semibold">
+          {host.full_name}'s Confirmed Information
+        </h3>
+        <div className="px-4 pt-2">
+          <div className="flex gap-2 border-b border-gray-300 px-2 py-2">
+            {host?.profession ? (
+              <SiVerizon />
+            ) : (
+              <RxCross2 className="text-lg text-red-500" />
+            )}
+            <p className="text-sm">Profession</p>
+          </div>
+          <div className="flex gap-2 border-b border-gray-300 px-2 py-2">
+            {host?.email ? (
+              <SiVerizon />
+            ) : (
+              <RxCross2 className="text-lg text-red-500" />
+            )}
+            <p className="text-sm">Email</p>
+          </div>
+          <div className="flex gap-2 border-b border-gray-300 px-2 py-2">
+            {host?.phone ? (
+              <SiVerizon />
+            ) : (
+              <RxCross2 className="text-lg text-red-500" />
+            )}
+            <p className="text-sm">Phone</p>
+          </div>
+          <div className="flex gap-2 px-2 py-2">
+            {host?.address ? (
+              <SiVerizon />
+            ) : (
+              <RxCross2 className="text-lg text-red-500" />
+            )}
+            <p className="text-sm">Address</p>
+          </div>
+        </div>
+      </div>
+      <Link href={"/"} className="my-5 flex items-center gap-2">
+        <HiMiniFlag className="text-sm" />
+        <span className="text-xs font-bold underline">Report this user</span>
+      </Link>
+    </div>
+  );
+}
+
+export default ProfileInfoCard;
