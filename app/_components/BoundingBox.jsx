4a4cda76c076d92191ef931c0a45c4d9c9ddd0bf
turja <zahidulturja@gmail.com>
2025-07-08 13:06:00 +0600

    fix: Adjust padding in BoundingBox and SectionHeader components for better responsiveness

--- app/_components/BoundingBox.jsx
+++ app/_components/BoundingBox.jsx
@@ -1,5 +1,5 @@
 function BoundingBox({ children, classes }) {
-  return <div className={`px-3 md:px-24 ${classes}`}>{children}</div>;
+  return <div className={`px-3 md:px-16 ${classes}`}>{children}</div>;
 }
 
 export default BoundingBox;
